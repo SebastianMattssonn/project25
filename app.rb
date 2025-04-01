c72635f505cad96c33c38d684e5207f3b30ecdc2
SebastianMattssonn <sebastian.mattsson@elev.ga.ntig.se>
2025-04-01 09:23:32 +0200

    Register och log in

--- app.rb
+++ app.rb
@@ -10,54 +10,72 @@ enable :sessions
 # Startsida
 get '/' do
   featured_products = DB.execute("SELECT * FROM Products ORDER BY RANDOM() LIMIT 4")
-  slim :home, locals: { products: featured_products }
+  
+  if session[:user_id]
+    user = DB.execute("SELECT Username FROM Users WHERE User_Id = ?", [session[:user_id]]).first
+    username = user ? user["Username"] : nil
+  else
+    username = nil
+  end
+
+  slim :home, locals: { products: featured_products, username: username }
 end
 
-#Produktsida
+# Produktsida
 get '/shop' do
   products = DB.execute("SELECT * FROM Products")
   slim :"products/index", locals: { products: products }
 end
 
-#Enskild produktsida
+# Enskild produktsida
 get '/products/:id' do
   product = DB.execute("SELECT * FROM Products WHERE Product_Id = ?", params[:id]).first
   slim :"products/show", locals: { product: product }
 end
 
-#Registrering
+# Registrering
 get '/register' do
-  slim :"users/register"
+  slim :"auth/register"
 end
 
 post '/register' do
   username = params[:username]
   password = params[:password]
+
+  if username.empty? || password.empty?
+    redirect '/register'
+  end
+
   password_digest = BCrypt::Password.create(password)
 
-  DB.execute("INSERT INTO Users (Username, Password) VALUES (?, ?)", [username, password_digest])
-  redirect '/login'
+  begin
+    DB.execute("INSERT INTO Users (Username, Password) VALUES (?, ?)", [username, password_digest])
+    redirect '/login'
+  rescue SQLite3::ConstraintException
+    redirect '/register' # Hanterar om användarnamnet redan finns
+  end
 end
 
-#Inloggning
+# Inloggning
 get '/login' do
-  slim :"users/login"
+  slim :"auth/login"
 end
 
 post '/login' do
   username = params[:username]
   password = params[:password]
-  
+
   user = DB.execute("SELECT * FROM Users WHERE Username = ?", [username]).first
+
   if user && BCrypt::Password.new(user["Password"]) == password
     session[:user_id] = user["User_Id"]
-    redirect '/shop'
+    redirect '/'
   else
     redirect '/login'
   end
 end
 
-#Utloggning
+# Utloggning
 get '/logout' do
   session.clear
   redirect '/'
